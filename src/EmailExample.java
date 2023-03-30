08cc7485d38146867bafcafa41e2d71e7d4bd605
DoChuan13 <chuando.9213@gmail.com>
2023-03-30 17:31:26 +0700

    Update

---
+++ src/EmailExample.java
@@ -0,0 +1,17 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class EmailExample {
+    private static final String EMAIL_REGEX = "^[A-Za-z0-9]+[A-Za-z0-9]*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)$";
+    private static Pattern pattern;
+    private Matcher matcher;
+
+    public EmailExample() {
+        pattern = Pattern.compile(EMAIL_REGEX);
+    }
+
+    public boolean validate(String regex) {
+        matcher = pattern.matcher(regex);
+        return matcher.matches();
+    }
+}
